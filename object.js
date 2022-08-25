fc916b2211c4f2058be229ec802b373e3f296907
zed <96680664+zuhrull@users.noreply.github.com>
2022-08-25 11:01:28 +0700

    Create object.js

---
+++ object.js
@@ -0,0 +1,15 @@
+let array = ['arief', 1, true, null]
+
+
+let stnk = {
+  tipeKendaraan: 'honda vario',
+  tipeMesin: 'matic',
+  tanggalPembuatan: '12-Juni',
+  name: 'Rudi Hartono'
+}
+
+// tipeKendaraan sebagai key
+// 'honda vario' sebagai value
+
+console.log('tipe kendaraan ====>', stnk.tipeKendaraan);
+console.log('tip mesin ====>', stnk['tipeMesin']);
